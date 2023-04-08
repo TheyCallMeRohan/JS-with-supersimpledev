e72adb4d7c0da1fd75456d209eb9a661f7c1f751
Younggmonk <rohanrawat2412@gmail.com>
2023-04-08 12:28:04 +0530

    TOS

---
+++ TOP.js
@@ -0,0 +1,62 @@
+// function getComputerChoice() {
+//   let computerSelection = Math.floor(Math.random() * 3);
+// }
+
+function playRound(playerSelection, computerSelection) {
+  let value = " ";
+  if (
+    playerSelection.toUpperCase() === "rock".toUpperCase() &&
+    computerSelection === 0
+  ) {
+    value = "It's a Tie";
+  } else if (
+    playerSelection.toUpperCase() === "paper".toUpperCase() &&
+    computerSelection === 0
+  ) {
+    value = "You Win! Paper beats Rock";
+  } else if (
+    playerSelection.toUpperCase() === "scissor".toUpperCase() &&
+    computerSelection === 0
+  ) {
+    value = "You Lose! Rock beats scissor";
+  } else if (
+    playerSelection.toUpperCase() === "rock".toUpperCase() &&
+    computerSelection === 1
+  ) {
+    value = "You Lose! Paper beats rock";
+  } else if (
+    playerSelection.toUpperCase() === "paper".toUpperCase() &&
+    computerSelection === 1
+  ) {
+    value = "It's a Tie";
+  } else if (
+    playerSelection.toUpperCase() === "scissor".toUpperCase() &&
+    computerSelection === 1
+  ) {
+    value = "You Win! scissor beats paper";
+  } else if (
+    playerSelection.toUpperCase() === "rock".toUpperCase() &&
+    computerSelection === 2
+  ) {
+    value = "You Win! Rock beats scissor";
+  } else if (
+    playerSelection.toUpperCase() === "paper".toUpperCase() &&
+    computerSelection === 2
+  ) {
+    value = "You Lose! Scissor beats paper";
+  } else if (
+    playerSelection.toUpperCase() === "scissor".toUpperCase() &&
+    computerSelection === 2
+  )
+    return value;
+}
+for (let i = 0; i < 5; i++) {
+  let playerSelection = prompt("Enter Rock Paper Scissor");
+  const computerSelection = Math.floor(Math.random() * 3);
+  const rock = 0;
+  const paper = 1;
+  const scissor = 2;
+  console.log(computerSelection);
+  console.log(playerSelection);
+  alert(playRound(playerSelection, computerSelection));
+}
